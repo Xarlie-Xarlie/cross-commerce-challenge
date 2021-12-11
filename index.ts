adc7a036ceb27e58ac57ea51ec7d8ccf8c157e06
Charlie Charlie <jeancharles552@gmail.com>
2021-12-11 18:20:28 -0300

    extract and transform improved with semaphores and quicksort

--- index.ts
+++ index.ts
@@ -1,58 +1,50 @@
 import cluster from 'cluster';
 import { cpus } from 'os';
 import { requestData, api_URL } from './extract';
-import { mergeSortTopDown } from './transform';
+import { quickSort } from './transform';
 import process from 'process';
+
 const numCPUs = cpus().length;
 
 interface dataProcess {
   message: string;
   fromPage: number;
   toPage: number;
-  lastPage: number;
 }
-
-// let teste = [
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-// ];
-
-// var newArray = [];
-// teste.forEach(element => {
-//   element.forEach(item => newArray.push(item));
-// });
-// teste = [...newArray];
 
 if (cluster.isPrimary) {
   //log Main Thread process
   console.log(`Primary ${process.pid} is running`);
 
+  var semaphoreRequests = require('semaphore')(1); // semaphore to deal with shared memory
+  var semaphoreLastPage = require('semaphore')(1); // semaphore to deal with shared memory
   const requests = []; // array to save requests
+  var finishedRequest = 0; // variable to track how many process are ended
 
   // Fork workers.
   var worker;
-  var lastPage = -1;
+  var lastPage = -1; // save the last page request in order to make more requests
   for (let i = 0; i < numCPUs; i++) {
-    if (i === numCPUs - 1) lastPage = 2500 + i * 2500;
-    // send a message to workers make the request with 2500 pages
+    if (i === numCPUs - 1) lastPage = 250 + i * 250;
+    //create a new worker
     worker = cluster.fork();
 
+    // send a message to workers make the request with 250 pages
     worker.send({
-      // since my cpu has 4 cores, i use 10000/numcpus = 2500 pages per core
       message: 'get data from api',
-      fromPage: 2500 * i + 1,
-      toPage: 2500 + i * 2500,
-      lastPage: lastPage,
+      fromPage: 250 * i + 1,
+      toPage: 250 + i * 250,
     });
 
+    //if a worker fisished it's request, it will save data received
     worker.on('message', data => {
       if (data.result) {
-        data.result.forEach(element => {
-          element.forEach(item => requests.push(item));
+        semaphoreRequests.take(() => {
+          data.result.forEach(element => {
+            element.forEach(item => requests.push(item));
+          });
         });
+        semaphoreRequests.leave();
       }
     });
   }
@@ -69,22 +61,40 @@ if (cluster.isPrimary) {
       );
     }
 
-    if (code === 2) {
-      // if code is 2 it means that we don't get an empty array from requests
-      cluster.fork().send({
-        message: 'still have data to request',
-        fromPage: lastPage,
-        toPage: lastPage + 2500,
-        lastPage: lastPage + 2500,
+    // if code is 1 it means that we don't get an empty array from requests
+    if (code === 1) {
+      // then create a new worker to make more requests
+      worker = cluster.fork();
+
+      worker.send({
+        message: 'get data from api',
+        fromPage: lastPage + 1,
+        toPage: lastPage + 250,
       });
-      lastPage += 2500;
+
+      // update the last page requested
+      semaphoreLastPage.take(() => {
+        lastPage += 250;
+        semaphoreLastPage.leave();
+      });
+
+      worker.on('message', data => {
+        if (data.result) {
+          semaphoreRequests.take(() => {
+            data.result.forEach(element => {
+              element.forEach(item => requests.push(item));
+            });
+            semaphoreRequests.leave();
+          });
+        }
+      });
     }
-    if (code === 3) {
+    // ended all requests receiving an empty array
+    if (code === 0) {
+      finishedRequest++;
       console.log(`requests.length: ${requests.length}`);
-      console.time('Time to sort items with MergeSort');
-      var sortedrequests = mergeSortTopDown(requests);
-      console.timeEnd('Time to sort items with MergeSort');
-      console.log(sortedrequests);
+      // if all process have finished, we compute the sort
+      if (finishedRequest === numCPUs) console.log(quickSort(requests));
     }
   });
 } else {
@@ -97,39 +107,17 @@ if (cluster.isPrimary) {
         data.toPage
       );
 
+      // send the results with an message to Primary process
       if (result.length > 0) {
         result = result[0].map(element => element.value.data.numbers);
         process.send({ result });
       }
 
       // if still have pages the process will restart
-      if (hasPages) process.exit(2);
-      // if doesn't have, it will just log the receive data
-      else if (!hasPages && result.length > 0) {
-        process.exit(0);
-      }
-      // it has no data remaining
-      else process.exit(3);
-    } else if (data.message === 'still have data to request') {
-      // request data in process
-      let { result, hasPages } = await requestData(
-        api_URL,
-        data.fromPage,
-        data.toPage
-      );
+      if (hasPages) process.exit(1);
 
-      if (result.length > 0) {
-        result = result[0].map(element => element.value.data.numbers);
-        process.send({ result });
-      }
-
-      // if still have pages to request the process will restart
-      if (hasPages) process.exit(2);
-      // if doesn't, it will just log the receive data
-      else if (!hasPages && result.length > 0) {
-        // console.log(resultFinal);
-        process.exit(3);
-      } else process.exit(0); // it has no data remaining
+      // if has no page more, it ends the process
+      process.exit(0);
     }
   });
 }
